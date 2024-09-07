a45ec2cc8a8b9d14d519da2b8ba2719f2aaa0778
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-09-07 14:35:55 +0300

    create db

---
+++ database.js
@@ -0,0 +1,17 @@
+import sqlite3 from "sqlite3";
+
+sqlite3.verbose();
+
+export const db = new sqlite3.Database("memory", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+
+  console.log("Connected to sqlite3 database");
+});
+
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXIST items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT)"
+  );
+});
