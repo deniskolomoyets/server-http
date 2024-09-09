af96b1aca25c37c1c2e2fec3e5759d5f0541ea24
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-09-09 19:47:22 +0300

    add controller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -11,3 +11,20 @@ export const getItems = async (req, res) => {
     res.end(`Error: ${err.message}`);
   }
 }; //getItems - Model call, and within the model, a call to the database
+
+export const getItemById = async (req, res, id) => {
+  try {
+    const item = await itemModel.getItemById(id);
+
+    if (!item) {
+      res.statusCode = 404;
+      res.end(`Item with ID: ${id} not found`);
+    } else {
+      res.setHeader("Content-type", "application/json");
+      res.end(JSON.stringify(item));
+    }
+  } catch (err) {
+    res.statusCode = 500;
+    res.end(`Error: ${err.message} `);
+  }
+};
